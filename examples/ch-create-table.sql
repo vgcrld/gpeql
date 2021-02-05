53bfe99fe735e24b722061c77d1c8275b3681719
Rich Davis <rdavis@ip-192-168-1-55.ec2.internal>
2021-02-05 17:30:43 -0500

    item types

--- examples/ch-create-table.sql
+++ examples/ch-create-table.sql
@@ -3,22 +3,28 @@
       Create the ch to mysql connection and merge in config
 
  */
-DROP TABLE IF EXISTS `data__rdavis2`.`mysql_config_names`;
+DROP TABLE IF EXISTS `data__atsgroup`.`config_with_names`;
 
-CREATE TABLE `data__rdavis2`.`mysql_config_names` (
+CREATE TABLE `data__atsgroup`.`config_with_names` (
   `id` int unsigned,
   `string` VARCHAR(255),
   `insert_ts` TIMESTAMP
-) ENGINE MySQL('core-pxc:3306', 'data__rdavis2', 'config_names', 'rdavis', 'qJS@H!sFOMmy' );
+) ENGINE MySQL('core-pxc:3306', 'data__atsgroup', 'config_names', 'rdavis', 'qJS@H!sFOMmy' );
 
+
+-- select configs with names
 SELECT
     v.type AS `type`,
     v.item_id AS `item_id`,
     n.string AS `config`,
     max(v.insert_ts) AS `update_ts`,
     argMax(v.config_value,v.insert_ts)  AS `value`
-FROM __configs AS v, mysql_config_names AS n
+FROM __configs AS v, config_with_names AS n
 WHERE n.id = v.config_id 
 GROUP BY `type`, `item_id`, `config`
 ORDER BY `item_id`, `config`
 ;
+
+
+-- all the item types and their tags
+select type, tags, count(*) as count from __items group by type, tags order by type;
--- src/index.js
+++ src/index.js
@@ -1,37 +1,36 @@
+// Basic requires
 const fs = require("fs");
 const path = require("path");
 
+// Apollo support, and clickhouse
 const { ApolloServer } = require("apollo-server");
+const GpeClickhouse = require("./pkg/clickhouse");
+const GpeMySQL = require("./pkg/mysql");
 
+// Other setup
+const p = (p) => console.log(p);
 const gpeDatabase = "data__atsgroup";
 
-const p = (p) => console.log(p);
-
-// clickhouse connection as a class
-const GpeClickhouse = require("./clickhouse");
+// MySQL and ClickHouse connection as a class
 ch = new GpeClickhouse(gpeDatabase);
-
-// clickhouse connection as a class
-const GpeMySQL = require("./mysql");
 ms = new GpeMySQL(gpeDatabase);
 
 // Store the resolvers here
 const resolvers = { Query: {} };
 
 // Resolver to get config names from the pxc database
-resolvers.Query.configs =
-  resolvers.Query.config_names = (parent, { sql }, context, info) => {
-    return new Promise(function (resolve, reject) {
-      let fullSql = `select * from config_names ${sql}`;
-      ms.connection.query(fullSql, (error, result, fields) => {
-        if (error) {
-          p(error.sqlMessage)
-        } else {
-          resolve(result);
-        }
-      });
+resolvers.Query.configs = resolvers.Query.config_names = ( parent, { sql }, context, info ) => {
+  return new Promise(function (resolve, reject) {
+    let fullSql = `select * from config_names ${sql}`;
+    ms.connection.query(fullSql, (error, result, fields) => {
+      if (error) {
+        p(error.sqlMessage);
+      } else {
+        resolve(result);
+      }
     });
-  };
+  });
+};
 
 // Resolver to get an individual item
 resolvers.Query.item = (parent, args, context, info) => {
@@ -46,16 +45,35 @@ resolvers.Query.items = (parent, { sql }, context, info) => {
 
 // Resolver to get all configs, limit with sql: param
 resolvers.Query.configs = (parent, { sql }, context, info) => {
-  sql = `
-    select type,item_id,config_id,argMax(config_value,insert_ts) config_value
-    from __configs ${sql} 
-    group by type,item_id,config_id;`;
+  sql = ` select type,item_id,config_id,argMax(config_value,insert_ts) config_value
+          from __configs ${sql}
+          group by type,item_id,config_id;`;
+  return ch.query(sql);
+};
+
+// Item Type and Tags
+resolvers.Query.item_info = (parent, { where, limit, having }, context, info) => {
+  console.log(typeof limit,'limit',limit)
+  let whereClause = ''
+  let limitClause = ''
+  whereClause  = (where === '')   ? '' : whereClause =  `where  ${where}`;
+  limitClause  = (limit === 0 )   ? '' : limitClause =  `limit  ${limit}`;
+  havingClause = (having === '' ) ? '' : havingClause = `having ${having}`;
+  sql = ` select type, tags, count(*) as count
+          from __items
+          ${whereClause}
+          group by type, tags
+          ${havingClause}
+          order by type
+          ${limitClause};
+  `
+  console.log(sql);
   return ch.query(sql);
 };
 
 // Let's start up the apllow server.
 const server = new ApolloServer({
-  typeDefs: fs.readFileSync(path.join(__dirname, "schema.graphql"), "utf8"),
+  typeDefs: fs.readFileSync(path.join(__dirname, "pkg/schema.graphql"), "utf8"),
   resolvers,
   // context: { ch, ms }
 });
